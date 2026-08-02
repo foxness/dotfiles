2d84efa9886ba8a451f233ce3617ee63a981ebe3
River <foxnezz@gmail.com>
2026-08-02 16:36:16 +0300

    ADD: add strikethrough zero and other stuff to jetbrains mono

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -42,6 +42,7 @@ vim.pack.add({
     { src = 'https://github.com/UtkarshVerma/molokai.nvim' },
     { src = 'https://github.com/rose-pine/neovim',                       name = 'rose-pine' },
     { src = 'https://github.com/sainnhe/sonokai' },
+    { src = 'https://github.com/maxmx03/solarized.nvim' },
 })
 
 -- ========== PLUGIN CONFIG ==========
@@ -233,6 +234,7 @@ require('gruvbox').setup {
 }
 
 vim.g.nightflyTransparent = true
+vim.g.moonflyTransparent = true
 vim.g.sonokai_transparent_background = true
 vim.g.sonokai_style = 'andromeda'
 
@@ -304,7 +306,7 @@ vim.api.nvim_create_autocmd("ColorScheme", {
     end,
 })
 
-vim.cmd.colorscheme('tokyonight-night')
+vim.cmd.colorscheme('gruvbox')
 
 -- ========== IMPORT ==========
 
